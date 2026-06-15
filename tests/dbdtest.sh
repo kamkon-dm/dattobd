4cbf6989f1950bac7216a5a4e39d062c4b0f682e
Kamil Kontorski <kamil.kontorski@gmail.com>
2026-06-15 15:28:49 +0200

    Add driver version report in tests

--- tests/dbdtest.sh
+++ tests/dbdtest.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # SPDX-License-Identifier: GPL-2.0-only
 
+set -u
+
 #
 # Copyright (C) 2019 Datto, Inc.
 #
 
+DBD_VER=$(grep -i "Version:" ../dist/dattobd.spec | awk '{print $2}')
+
 echo
-echo "dattobd: $(git rev-parse --short HEAD)"
+echo "dattobd: $DBD_VER $(git rev-parse --short HEAD)"
 echo "kernel: $(uname -r)"
 echo "gcc: $(gcc --version | awk 'NR==1 {print $3}')"
 echo "bash: ${BASH_VERSION}"
